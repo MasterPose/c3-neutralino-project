8f1bdf638e2749db9db822627a9f1465718b3949
Luan Himmlisch <luan@himmlisch.com.mx>
2025-07-25 20:39:33 -0600

    Inherit URL params to Construct iframe

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     /** @type {HTMLIFrameElement} */
     const iframe = document.getElementById('game');
+    const inheritParams = new URL(document.location.toString()).searchParams;
     const params = new URLSearchParams({
+        // Pass the query params to the iFrame
+        // This is needed for multi-window applications
+        ...inheritParams,
+        // Generate random values to prevent
+        // the client browser to cache
         timestamp: Date.now()
     });
 
-    // Generate random values to prevent
-    // the client browser to cache
 
     iframe.name = Math.round(Math.random() * 10000);
     iframe.src = `./game/index.html?${params}`;
